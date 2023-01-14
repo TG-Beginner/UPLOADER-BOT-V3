75b4d6580a518033ed1b98279cccccd3e54e237b
anumitultra <105001750+anumitultra@users.noreply.github.com>
2023-01-14 10:12:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ git clone https://github.com/anumitultra/UPLOADER-BOT-V3 /UPLOADER-BOT-V3
 cd /UPLOADER-BOT-V3
 pip3 install -U -r requirements.txt
 echo "Starting Bot.... Please Wait. Check You Log. Bot Edit by @LISA_FAN_KL"
-gunicorn app:app & python3 bot.py
+python3 bot.py
